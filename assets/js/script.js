d8ef56877710328f903125f76a5362b82b6a320f
dagon8523 <dagon8523@gmail.com>
2020-08-25 19:01:36 -0500

    se termina etilo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,25 @@
-var divperfil=document.getElementById("perfil")
+var divboton=document.getElementById("boton")
 var divnombre=document.getElementById("nombre")
 var divnacionalidad=document.getElementById("nacionalidad")
 var divprofesion=document.getElementById("profesion")
 var divtrabajos=document.getElementById("trabajos-musicales")
 
-divperfil.addEventListener('click',()=>{
-    console.log("este es el perfil vacio")
-    console.log(divperfil)
+divboton.addEventListener('click',llenardatos)
 
-    var jsnombre=prompt("digita el nombre de cancerbero")
-    var jsnacionalidad=prompt("digita nacionalidad de cancerbero")
-    var jsprofesion=prompt("digita la profesion de cancerbero")
-    var jstrabajos=prompt("digita los trabajos de cancerbero")
 
+
+
+function llenardatos(){
+   
+     var jsnombre=prompt("digita el nombre de cancerbero")
     divnombre.textContent=jsnombre;
+    var jsnacionalidad=prompt("digita pais nacimiento de cancerbero")
     divnacionalidad.textContent=jsnacionalidad;
+    var jsprofesion=prompt("digita la profesion de cancerbero")
     divprofesion.textContent=jsprofesion;
+    var jstrabajos=prompt("digita los trabajos de cancerbero")
     divtrabajos.textContent=jstrabajos;
 
-})
 
-
-
-
-/* function llenardatos(){
-    console.log("este es el perfil vacio")
-    console.log(divperfil)
-
-    var jsnombre=prompt("digita el nombre de cancerbero")
-    var jsnacionalidad=prompt("digita nacionalidad de cancerbero")
-    var jsprofesion=prompt("digita la profesion de cancerbero")
-    var jstrabajos=prompt("digita los trabajos de cancerbero")
-
-    divnombre.textContent=jsnombre;
-    divnacionalidad.textContent=jsnacionalidad;
-    divprofesion.textContent=jsprofesion;
-    divtrabajos.textContent=jstrabajos;
-
-} */
+} 
 
